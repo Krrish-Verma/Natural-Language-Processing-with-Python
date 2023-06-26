a4336c3344e38e48ba54c5836d14e01b4d91482e
Krrish Verma <96752255+Krrish-Verma@users.noreply.github.com>
2023-06-26 12:57:31 -0500

    Delete main.py
    not needed

--- main.py
+++
@@ -1,7 +0,0 @@
-from nltk.corpus import wordnet as wn
-
-#given a word lemma, synsets() function returns
-# list of Synset objects
-d = wn.synsets('dog')
-
-print(d) #will print a list of synsets that dog is associated with
